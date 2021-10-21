4516a1a8ca3dd242a3139fe500ea6fb97f270365
Akash Kumar <akash.kumar@hsrw.org>
2021-10-21 13:16:16 +0200

    adding files to deploy model

--- App.py
+++ App.py
@@ -15,7 +15,6 @@ def predict():
     Month=event['month']
     Year=event['year']
     p=int(model.forecast('%s-%s' % (Year,Month))[-1])
-    print(p)
     out = {'Prediction': p}
     return out
 
@@ -25,38 +24,3 @@ if __name__=='__main__':
 
 
 
-
-
-
-
-
-
-
-# =============================================================================
-# class Predict(Resource):
-# 
-#     @staticmethod
-#     def post():
-#         parser = reqparse.RequestParser()
-#         parser.add_argument('year')
-#         parser.add_argument('month')
-#        
-#         args = parser.parse_args()  # creates dict
-# 
-#         X_new = args.keys()  # convert input to array
-# 
-#         out = {'Prediction': MODEL.predict('%s-%s' % (X_new))}
-# 
-#         return out, 200
-# 
-# 
-# API.add_resource(Predict, '/predict')
-# 
-# if __name__ == '__main__':
-#     APP.run(debug=True, port='1080')
-#     
-# =============================================================================
-    
-    
-# model = pickle.load(open('model.pkl','rb'))
-# print(model.predict([[2, 9, 6]]))
